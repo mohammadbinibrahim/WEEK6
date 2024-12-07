c25235458266404de9aa9f0ea4f749747b24a899
mohammadbinibrahim <109195335+mohammadbinibrahim@users.noreply.github.com>
2024-12-08 01:52:36 +0200

    Add files via upload

---
+++ crud_js/main.js
@@ -0,0 +1,73 @@
+function add_success(){
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  };
+  toastr["success"]("User added successfully!")
+};
+
+function confirm_delete(id){
+  let del=confirm('Are you sure you want to delete the user');
+  if(del==true){
+    window.location.href='index.php?action=del&&id='+id; // redirect the browser to this URL
+  };
+};
+
+function del_success(){
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  };
+  toastr["error"]("User removed successfully!")
+};
+
+function edit(id){
+  window.location.href='add_user.php?action=edit&&id='+id; // redirect the browser to this URL
+};
+
+function edit_success(){
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  };
+  toastr["success"]("User edited successfully!")
+};
